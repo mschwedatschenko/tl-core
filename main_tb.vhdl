2a92d80201cd68704fd5f817966795fb93d1b394
mschwedatschenko <mschwedatschenko24@gmail.com>
2025-11-12 10:56:44 -0500

    test bench

--- main_tb.vhdl
+++ main_tb.vhdl
@@ -1,22 +1,56 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+-- Test bench for the SetVolts vhdl module
+-------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
+library UNISIM;
+use UNISIM.vcomponents.all;
 
 entity tl_test is
+
+
+end entity tl_test; --test
+
+architecture testbench of main_tb is
+    component design_1_wrapper is
     port (
         clk         : in  std_logic;
         rst         : in  std_logic;
         led_controller : out std_logic_vector(7 downto 0)
     );
+    end component design_1_wrapper;
 
-end entity tl_test; --test
+    signal clk       : std_logic                     := '0';
+    signal rst       : std_logic                     := '0';
+    signal led_controller       : std_logic_vector(7 downto 0):= (others => '0');
 
-architecture testbench of main_tb is
+  -- Clock periods
+  constant adc_clk_period : time := 8 ns;
+
 begin
-    stoplight : process
-    begin
+    uut : design_1_wrapper
+    port map (
+        clk       => adc_clk,
+        rst       => clk_rst,
+        led_controller => led_controller
+        );
 
-    end process stoplight;
+
+  adc_clk_process : process
+  begin
+    adc_clk <= '0';
+    wait for adc_clk_period/2;
+    adc_clk <= '1';
+    wait for adc_clk_period/2;
+  end process;
+
+
+--    stoplight : process
+--    begin
+
+--    end process stoplight;
 
 end architecture testbench; 
 
